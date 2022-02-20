42f7451f1ea4ae1b0bed94fcecb1b8892d7186ee
jiudianren <648087012@qq.com>
2022-02-20 19:43:06 +0800

    Create 877. 石子游戏.py

---
+++ 877. 石子游戏.py
@@ -0,0 +1,34 @@
+class Solution:
+    def stoneGame(self, piles: list) -> bool:
+        totoal_num = len(piles)
+        dp = []
+        for i in range(totoal_num):
+            dp.append([0]*totoal_num)
+        #print(dp)
+        for i in range(totoal_num):
+            dp[i][i] = piles[i]
+        #print(dp)
+        for i in range(totoal_num):
+            for j in range(i+1,totoal_num):
+
+                one_re = piles[i] - dp[i+1][j]
+                two_re = piles[j] - dp[i][j-1]
+
+                dp[i][j]= max(one_re,two_re)
+                print(f" i j {i} {j},bijiao {one_re} {two_re} ={dp[i][j]}" )
+                #print(f"re{dp[i][j]}")
+        #print(dp)
+        if dp[0][j] >0:
+            return True
+        else :
+            return False
+
+
+
+    def t(self):
+        piles= [3,7,2,3]
+        print(self.stoneGame(piles))
+
+
+
+Solution().t()
