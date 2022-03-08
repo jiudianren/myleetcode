ad6eb2711f8305ed909c4ebf00df030cebd2d767
jiudianren <648087012@qq.com>
2022-03-08 19:40:45 +0800

    Update 买卖股票的最佳时机 II.py

--- 买卖股票的最佳时机 II.py
+++ 买卖股票的最佳时机 II.py
@@ -1,22 +1,20 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-            totoal_num = len(piles)
-            dp = []
-            for i in range(totoal_num):
-                dp.append([0]*totoal_num)
-            #print(dp)
-            for i in range(totoal_num):
-                dp[i][i] = piles[i]
-            #print(dp)
-            for i in range(totoal_num):
-                for j in range(i+1,totoal_num):
-                    one_re = piles[i] - dp[i+1][j]
-                    two_re = piles[j] - dp[i][j-1]
-                    dp[i][j]= max(one_re,two_re)
-                    #print(f" i j {i} {j},bijiao {one_re} {two_re} ={dp[i][j]}" )
-                    #print(f"re{dp[i][j]}")
-            #print(dp)
-            if dp[0][totoal_num-1] :
-                return True
-            else :
-                return False
+       '''
+        dp[i][0]   第i天，不持有股票的最大收益
+        dp[i][1]   第i天， 持有一支股票的最大收益
+        买入算复数，卖出算正数
+        dp[i][0] = manx{  dp[i-1][0], dp[i-1][1] + prices[i]}
+        dp[i][1] = max{ dp[i-1][1], dp[i-1][0]-prices[i]  }
+        '''
+        dp = []
+        for i in range(len(prices)):
+            dp.append([])
+        #print(dp)
+        dp[0].append(0)
+        dp[0].append(-prices[0])
+        for i in range(1,len(prices)):
+            dp[i].append(max( dp[i-1][0],dp[i-1][1]+prices[i]))
+            dp[i].append(max( dp[i-1][1], dp[i-1][0]-prices[i]))
+        #print(dp)
+        return dp[len(prices)-1][0]
